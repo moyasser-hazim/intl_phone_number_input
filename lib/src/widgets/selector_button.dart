84c0707706646920e851edab2a0fb80713fd4449
moyasser hazim <measer100@gmail.com>
2024-01-23 12:41:57 +0300

    Revert "ref: add border to area code"
    This reverts commit 8eca70769f48610596a8108b033dd0acb08c0188.

--- lib/src/widgets/selector_button.dart
+++ lib/src/widgets/selector_button.dart
@@ -84,7 +84,7 @@ class SelectorButton extends StatelessWidget {
                   }
                 : null,
             child: Padding(
-              padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+              padding: const EdgeInsets.only(right: 8.0),
               child: Item(
                 country: country,
                 showFlag: selectorConfig.showFlags,
